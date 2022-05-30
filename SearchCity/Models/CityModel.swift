4c0f2957263d8bc5d3b293b013fdc9e9fe643d20
manish045 <manish@canbrand.in>
2022-05-30 14:44:10 +0400

    added cities json, write decode method for json, added model

---
+++ SearchCity/Models/CityModel.swift
@@ -0,0 +1,30 @@
+//
+//  CityModel.swift
+//  SearchCity
+//
+//  Created by Manish Tamta on 30/05/2022.
+//
+
+import Foundation
+
+// MARK: - CityModel
+
+typealias CitiesModel = [CityModel]
+
+struct CityModel: Codable {
+    let country: String?
+    let name: String?
+    let id: Int
+    let coord: Coord?
+    
+    enum CodingKeys: String, CodingKey {
+        case country, name
+        case id = "_id"
+        case coord
+    }
+}
+
+// MARK: - Coord
+struct Coord: Codable {
+    let lon, lat: Double
+}
