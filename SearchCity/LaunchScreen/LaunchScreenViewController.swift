5d9a119df1357e618564490d667abefe401d819b
manish045 <manish@canbrand.in>
2022-05-30 14:26:57 +0400

    initialize launch screen for basic computation

--- SearchCity/LaunchScreen/LaunchScreenViewController.swift
+++ SearchCity/LaunchScreen/LaunchScreenViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LaunchScreenViewController.swift
 //  SearchCity
 //
 //  Created by Manish Tamta on 30/05/2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LaunchScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
 }
-
