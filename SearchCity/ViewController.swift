28a2a09d2923a1681a0bf8be7f5d2d5dd23723c0
manish045 <manish@canbrand.in>
2022-05-30 14:21:08 +0400

    Initial Commit

---
+++ SearchCity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchCity
+//
+//  Created by Manish Tamta on 30/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
