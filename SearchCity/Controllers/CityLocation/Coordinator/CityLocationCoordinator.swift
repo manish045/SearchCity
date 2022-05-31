062f71d7aeee4402c1bf08664c5246f9fda8e6a0
manish045 <manish@canbrand.in>
2022-05-31 15:39:21 +0400

    link coordinator to controller and viewmodel

--- SearchCity/Controllers/CityLocation/Coordinator/CityLocationCoordinator.swift
+++ SearchCity/Controllers/CityLocation/Coordinator/CityLocationCoordinator.swift
@@ -12,4 +12,16 @@ protocol CityLocationOutputCoordinator {}
 class CityLocationCoordinator: Coordinator {
     var rootController: UIViewController?
     
+    func makeModule() -> UIViewController {
+        let vc = createViewController()
+        rootController = vc
+        return vc
+    }
+    
+    private func createViewController() -> CityLocationViewController {
+        let view = CityLocationViewController.instantiateFromStoryboard()
+        let viewModel = CityLocationViewModel()
+        view.viewModel = viewModel
+        return view
+    }
 }
